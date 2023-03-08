5ca018c41762dec18f24960f867df9fab56b3dd4
Ozeias Santos <ozzyton18@gmail.com>
2023-03-07 21:38:11 -0300

    Importacao

---
+++ type-boolean.js
@@ -0,0 +1,17 @@
+const primeiroNumero = 6;
+const segundoNumero = 5;
+
+// console.log(primeiroNumero === segundoNumero);
+
+const texto1 = ("Alura").toUpperCase();
+const texto2 = ("alura").toUpperCase();
+
+console.log(texto1 === texto2);
+
+/*
+if (primeiroNumero === segundoNumero) {
+    console.log("Verdadeiro");
+}
+else {
+    console.log("Falso");
+} */
---
+++ type-string.js
@@ -0,0 +1,18 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Ozeias";
+
+console.log(texto1);
+console.log(citacao + meuNome);
+
+// concatenação (+)
+
+
+
+
+// template string ou template literal
+
