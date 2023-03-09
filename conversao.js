a5192d2fa149b743475fa2ce0fdcab981838f541
Ozeias Santos <ozzyton18@gmail.com>
2023-03-08 21:59:54 -0300

    var e conversão

---
+++ conversao.js
@@ -0,0 +1,14 @@
+// tipo de dado
+// booleans
+
+// conversao implicita
+const numero = 456;
+const numeroString = "456";
+
+// funções
+// number() --> converte em numero
+// string() --> converte em string
+console.log(numero + Number(numeroString))
+
+// comversao explitica
+
